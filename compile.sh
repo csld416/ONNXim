d179588b2d8054bf8d1d70a6e1e5c5d018498c15
csld <chrischenscience@gmail.com>
2026-03-19 00:32:02 +0800

    update compile with build

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
-mkdir build && cd build
-conan install ..
-cmake ..
-make -j
-cd build/
-make
-cd ..
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d build ] || [ -z "$(ls -A build 2>/dev/null)" ]; then
+    mkdir -p build
+    cd build
+    conan install ..
+    cmake ..
+    make
+    cd ..
+else
+    cd build
+    make
+    cd ..
+fi
